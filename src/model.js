53633e4e46b715e2dc8f6fff4c4433ca926e6e79
Artak Harutyunyan <artak_harutyunyan@mail.com>
2020-09-21 16:07:13 +0400

    Creating constructor

--- src/model.js
+++ src/model.js
@@ -1,10 +1,11 @@
-import image from "./assets/matrix.png";
+import image from "./assets/image.png";
 import {
   TextBlock,
   TitleBlock,
   ImageBlock,
   TextColumnsBlock,
 } from "./classes/blocks";
+import { css } from "./utils";
 
 const text = `
 All my projects are here: <a href="https://github.com/artakharutyunyan" target="_blank">Artak Harutyunyan</a>. You will find very interesting projects: Chess, Tetris, Star Match Game etc.. Come and join!
@@ -13,12 +14,16 @@ All my projects are here: <a href="https://github.com/artakharutyunyan" target="
 export const model = [
   new TitleBlock("Website constructor on pure JavaScript!", {
     tag: "h2",
-    styles:
-      "background: linear-gradient(to right, #ff0099, #493240);color: #fff;padding: 1.5rem;text-align: center;",
+    styles: css({
+      background: "linear-gradient(to right, #ff0099, #493240)",
+      color: "#fff",
+      padding: "1.5rem",
+      "text-align": "center",
+    }),
   }),
   new ImageBlock(image, {
     styles: "padding: 2rem 0;display: flex;justify-content: center;",
-    alt: "matrix",
+    alt: "my image",
     imageStyles: "width: 500px; height: auto;",
   }),
   new TextColumnsBlock(
